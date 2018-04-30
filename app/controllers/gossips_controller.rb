e67d98f8dec1b581a69755d9273f93905c6b5e4a
Tatijana Rajcic <tatijana.rajcic@essec.edu>
2018-04-30 20:00:54 +0200

    Question 5 terminée

---
+++ app/controllers/gossips_controller.rb
@@ -0,0 +1,45 @@
+class GossipsController < ApplicationController
+
+  def new
+  	@gossip = Gossip.new
+  end
+
+  def create
+  	@gossip = Gossip.create(gossip_params)
+    redirect_to gossip_path(@gossip.id)
+  end
+
+  def show
+  	@gossip = Gossip.find(params[:id])
+  end
+
+
+
+  def edit
+    @gossip = Gossip.find(params[:id])
+  end
+
+  def update
+  	@gossip = Gossip.find(params[:id])
+  	@gossip.update(gossip_params)
+    redirect_to gossip_path(@gossip.id)
+  end
+
+  def destroy
+  	@gossip = Gossip.find(params[:id])
+  	@gossip.destroy
+  	redirect_to gossips_path
+  end
+
+  def index
+  	@gossips = Gossip.all
+  end
+
+
+  private
+
+  def gossip_params 
+	params.require(:gossip).permit(:anonymous_author, :content)
+  end
+
+end
